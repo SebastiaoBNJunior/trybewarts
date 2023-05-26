1151f2d4111817041ac7c979d5db757168683d65
vlademir-junior <vlademir.junior.nh@gmail.com.br>
2023-05-26 17:29:44 -0300

    resolução requisito 3

---
+++ script.js
@@ -0,0 +1,12 @@
+function validateForm() {
+  const email = document.getElementsByName('email')[0].value;
+  const password = document.getElementsByName('password')[0].value;
+
+  if (email === "" || password === "") {
+    alert("Email ou senha inválidos.");
+  } else {
+    alert("Olá, Tryber!");
+  }
+}
+
+validateForm();
