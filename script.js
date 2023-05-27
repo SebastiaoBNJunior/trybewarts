8b147fd558bbaccff5f78bc8dbbfee13b90f943d
vlademir-junior <vlademir.junior.nh@gmail.com.br>
2023-05-26 21:28:21 -0300

    resolução requisito 18

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const checkbox = document.getElementById('agreement');
+const submitBtn = document.getElementById('submit-btn');
+
+// eslint-disable-next-line func-names
+checkbox.addEventListener('change', function () { // REQUISITO 18
+  submitBtn.disabled = !this.checked;
+});
+
 function validateForm() {
   const email = document.getElementsByName('email')[0].value;
   const password = document.getElementsByName('password')[0].value;
